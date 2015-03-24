09c2b351467a7f24699c935195e0d679120e15df
unknown <pwest@WestP.csuniv.edu>
2015-03-24 05:12:17 -0400

    lab13 classs work

---
+++ src/lab13/Converter.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab13;
+
+/**
+ *
+ * @author pwest
+ */
+public class Converter {
+    public static Integer utoi (String str) {
+        Integer.parseInt(str);
+        
+        return null;
+    }
+}
