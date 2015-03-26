cd4f578db5c56ccdbfc1c5a2d6a542e905486312
unknown <pwest@WestP.csuniv.edu>
2015-03-26 05:11:44 -0400

    lab14 class files

---
+++ test/lab14/GCDTester.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab14;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author pwest
+ */
+public class GCDTester {
+    
+    public GCDTester() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    // @Test
+    // public void hello() {}
+    @Test
+    public void GCDTest() {
+        assertTrue(DivisorCalc.gcd(3, 9) == 3);
+        assertTrue(DivisorCalc.gcd(10, 15) == 5);
+    }
+}
